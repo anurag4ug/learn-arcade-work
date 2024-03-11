d75f238d84e6c115706a56a31cfde96dd03f8e66
anurag4ug <magartanurag@gmail.com>
2024-03-10 22:48:59 -0400

    Hii
    This was hard

---
+++ Testing/Advanced looping.py
@@ -0,0 +1,62 @@
+#problem 2
+
+#for i in range(10):
+#    print("*", end=" ")
+#print()
+#for i in range(5):
+#    print("*", end=" ")
+#print()
+#for i in range(20):
+#    print("*", end=" ")
+#print()
+
+#problem 3
+
+#for i in range(10):
+#    for j in range(5):
+#        print("*", end=" ")
+#    print()
+
+#problem 6
+
+#for i in range(10):
+#    for j in range(10):
+#        print(j , end=" ")
+#    print()
+
+#problem 7
+
+#for i in range(10):
+#    for j in range(10):
+#        print(i , end=" ")
+#    print()
+
+#problem 9
+
+#for i in range(10):
+#    for j in range(i):
+#        print(end="  ")
+#    for j in range(10-i):
+#       print(j, end=" ")
+#    print()
+
+#problem 10
+
+#for i in range(1, 10):
+#    for j in range (1,10):
+#        product = i * j
+#        if product < 10:
+#            print(" ", end="")
+#        print(product, end=" ")
+#    print()
+
+#problem 11
+
+#for i in range(1,10):
+#    for j in range(9-i,0,-1):
+#        print(end="  ")
+#    for j in range(i):
+#        print(j+1,end=" ")
+#    for j in range(j,0,-1):
+#        print(j,end=" ")
+#    print()
